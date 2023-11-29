7d388b97748b3edb05394bedff1728da611e801d
dauskkbd <dauskkbd@gmail.com>
2023-11-29 15:11:49 +0800

    added javascript file for practice and study

---
+++ Javascript/JS/calculator.js
@@ -0,0 +1,6 @@
+function runActivity() {
+  let first_number = Number(prompt("Give me a number"));
+  let second_number = Number(prompt("Give me another number"));
+  alert(first_number + second_number);
+  console.log(first_number + second_number);
+}
