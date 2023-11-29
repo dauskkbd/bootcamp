6244e13b0dd917e7ada1b4d12752803d60839ff7
dauskkbd <dauskkbd@gmail.com>
2023-11-29 10:37:59 +0800

    addedd javascript files

---
+++ Javascript/JS/hello.js
@@ -0,0 +1,6 @@
+function runActivity() {
+  //   alert("Hello, Kinut!");
+  let age = prompt("How old are you?");
+  console.log("Ah, you're " + age + " years old!");
+  //   console.error("ERROR: Incorrect Value!");
+}
