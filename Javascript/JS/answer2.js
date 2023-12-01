a4e67012a20e67a7b850550a113117eb3cea1445
dauskkbd <dauskkbd@gmail.com>
2023-12-01 16:54:12 +0800

    adding javascript files for studies

---
+++ Javascript/JS/answer2.js
@@ -0,0 +1,6 @@
+function runActivity() {
+  let n = Number(prompt("Give me a number"));
+  for (let i = 0; i < 2 * n; i++) {
+    console.log("Bacon");
+  }
+}
---
+++ Javascript/JS/counter.js
@@ -0,0 +1,17 @@
+function runActivity() {
+  let user_number;
+  let integer;
+  let decimal;
+  let even;
+  let odd;
+  let negative;
+  let positive;
+
+  let user_input = Number(prompt("Choose a number between 2 to 20"));
+  for (let i = 0; i < user_input; i++) {
+    if (user_input > 2 && user_input < 20) {
+      user_number = Number(prompt("Give me a number."));
+    }
+    console.log(i);
+  }
+}
